3c43e267b609a206343f267e96e2babc5cc26927
Alexey Borodov <dojdiq1@gmail.com>
2022-11-25 17:33:32 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = "5689021589:AAGHvgu7TzRPomwSStOJ3FwIc92GoZ4Rf2E"  # BitAlBorBot
+
+keys = {
+        "Ruble": "RUB",
+        "Euro": "EUR",
+        "Dollar": "USD",
+        "Hryvnia": "UAH",
+        "Yuan": "CNY"
+}
